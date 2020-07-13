8bf0cb87a20f4363f89480492d54560706ced54e
drop-out <drop-out@foxmail.com>
2020-07-13 08:33:28 +0800

    add feature: generate nick automatically
    add feature: generate nick automatically
    add feature: create table

--- SQLGenerator.py
+++ SQLGenerator.py
@@ -1,14 +1,36 @@
+alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 class SQLGenerator:
-    def __init__(self,input_name,output_name,output_nick,select=None,where=None):
+    nick_list = ['ADD','ALL','ALTER','AND','AS','ASC','BETWEEN','BIGINT','BOOLEAN','BY','CASE','CAST','COLUMN','COMMENT','CREATE','DESC','DISTINCT','DISTRIBUTE','DOUBLE','DROP','ELSE','FALSE','FROM','FULL','GROUP','IF','IN','INSERT','INTO','IS','JOIN','LEFT','LIFECYCLE','LIKE','LIMIT','MAPJOIN','NOT','NULL','ON','OR','ORDER','OUTER','OVERWRITE','PARTITION','RENAME','REPLACE','RIGHT','RLIKE','SELECT','SORT','STRING','TABLE','THEN','TOUCH','TRUE','UNION','VIEW','WHEN','WHERE']
+    suggest_pick = 0
+    
+    def reset_nick():
+        SQLGenerator.nick_list = ['ADD','ALL','ALTER','AND','AS','ASC','BETWEEN','BIGINT','BOOLEAN','BY','CASE','CAST','COLUMN','COMMENT','CREATE','DESC','DISTINCT','DISTRIBUTE','DOUBLE','DROP','ELSE','FALSE','FROM','FULL','GROUP','IF','IN','INSERT','INTO','IS','JOIN','LEFT','LIFECYCLE','LIKE','LIMIT','MAPJOIN','NOT','NULL','ON','OR','ORDER','OUTER','OVERWRITE','PARTITION','RENAME','REPLACE','RIGHT','RLIKE','SELECT','SORT','STRING','TABLE','THEN','TOUCH','TRUE','UNION','VIEW','WHEN','WHERE']
+        SQLGenerator.suggest_pick = 0
+    
+    def __init__(self,input_table,where=None,select=None):
+        while True:
+            nick = []
+            current_pick = SQLGenerator.suggest_pick
+            nick.append(alphabet[current_pick%26])
+            current_pick = current_pick//26
+            while current_pick>0:
+                current_pick = current_pick//26
+                nick.append(alphabet[current_pick%26])
+            nick = ''.join(nick[::-1])
+            if nick not in SQLGenerator.nick_list:
+                SQLGenerator.nick_list.append(nick)
+                self.nick = nick
+                break
+            else:
+                SQLGenerator.suggest_pick+=1
+                
         if select is None:
             select = '*'
         else:
             select = '\n,'.join(select)
         if where is None:
             where = 'True'
-        self.sql_string = 'select\n%s\nfrom %s\nwhere %s'%(select,input_name,where)
-        self.output_nick = output_nick
-        self.output_name = output_name
+        self.sql_string = 'select\n%s\nfrom %s\nwhere %s'%(select,input_table,where)
         self.current_layer = 0
 
     def select(self,select,where=None):
@@ -16,7 +38,7 @@ class SQLGenerator:
             where = 'True'
         self.sql_string = '\n'.join(['\t'+line for line in self.sql_string.split('\n')])
         select_string = '\n,'.join(select)
-        layer_name = '%s_%s'%(self.output_nick,self.current_layer)
+        layer_name = '%s_%s'%(self.nick,self.current_layer)
         self.sql_string = '''select 
 %s
 from
@@ -29,7 +51,7 @@ where %s'''%(select_string,self.sql_string,layer_name,where)
         
     def where(self,where):
         self.sql_string = '\n'.join(['\t'+line for line in self.sql_string.split('\n')])
-        layer_name = '%s_%s'%(self.output_nick,self.current_layer)
+        layer_name = '%s_%s'%(self.nick,self.current_layer)
         self.sql_string = '''select 
 *
 from
@@ -43,7 +65,7 @@ where %s'''%(self.sql_string,layer_name,where)
     def rename(self,original_name,new_name):
         self.sql_string = '\n'.join(['\t'+line for line in self.sql_string.split('\n')])
         rename_string = '%s as %s'%(original_name,new_name)
-        layer_name = '%s_%s'%(self.output_nick,self.current_layer)
+        layer_name = '%s_%s'%(self.nick,self.current_layer)
         self.sql_string = '''select 
 *
 ,%s
@@ -57,7 +79,7 @@ from
     def rename_multiple(self,rename_struct):
         self.sql_string = '\n'.join(['\t'+line for line in self.sql_string.split('\n')])
         rename_string = '\n,'.join(['%s as %s'%(i[0],i[1]) for i in rename_struct])
-        layer_name = '%s_%s'%(self.output_nick,self.current_layer)
+        layer_name = '%s_%s'%(self.nick,self.current_layer)
         self.sql_string = '''select 
 *
 ,%s
@@ -72,7 +94,7 @@ from
     def UDF(self,UDF_name,input_name_list,output_name):
         self.sql_string = '\n'.join(['\t'+line for line in self.sql_string.split('\n')])
         UDF_string = '%s(%s) as %s'%(UDF_name,','.join(input_name_list),output_name)
-        layer_name = '%s_%s'%(self.output_nick,self.current_layer)
+        layer_name = '%s_%s'%(self.nick,self.current_layer)
         self.sql_string = '''select 
 *
 ,%s
@@ -86,7 +108,7 @@ from
     def UDF_multiple(self,UDF_struct):
         self.sql_string = '\n'.join(['\t'+line for line in self.sql_string.split('\n')])
         UDF_string = '\n,'.join(['%s(%s) as %s'%(i[0],','.join(i[1]),i[2]) for i in UDF_struct])
-        layer_name = '%s_%s'%(self.output_nick,self.current_layer)
+        layer_name = '%s_%s'%(self.nick,self.current_layer)
         self.sql_string = '''select 
 *
 ,%s
@@ -100,8 +122,8 @@ from
     def join(self,target_table_sql_generator,on,how='left outer'):
         self.sql_left = '\n'.join(['\t'+line for line in self.sql_string.split('\n')])
         self.sql_right = '\n'.join(['\t'+line for line in target_table_sql_generator.sql_string.split('\n')])
-        self.layer_name_left = '%s_%s'%(self.output_nick,self.current_layer)
-        self.layer_name_right = '%s_%s'%(target_table_sql_generator.output_nick,target_table_sql_generator.current_layer)
+        self.layer_name_left = '%s_%s'%(self.nick,self.current_layer)
+        self.layer_name_right = '%s_%s'%(target_table_sql_generator.nick,target_table_sql_generator.current_layer)
         self.sql_on_condition = on.replace('left',self.layer_name_left).replace('right',self.layer_name_right)
         self.sql_string = '''select 
 *
@@ -130,16 +152,16 @@ on %s'''%(self.sql_left,self.layer_name_left,how,self.sql_right,self.layer_name_
         return self.join(target_table_sql_generator,on,how='full outer')
     
     
-    def get_sql(self,drop = False):
-        self.sql_string = 'create table %s as \n%s' %(self.output_name,self.sql_string)+'\n;'
+    def create_table(self,table_name,drop = False):
+        self.sql_string = 'create table %s as \n%s' %(table_name,self.sql_string)+'\n;'
         if drop:
-            self.sql_string = 'drop table if exists %s;\n'%self.output_name+self.sql_string
+            self.sql_string = 'drop table if exists %s;\n'%table_name+self.sql_string
         print(self.sql_string)
 
     
 
-table_left = SQLGenerator('the_from_table','table_final','tt','dt=20200101')
-table_right = SQLGenerator('the_right_from_table','','bb','dt=20200101')
+table_left = SQLGenerator('the_from_table','dt=20200101')
+table_right = SQLGenerator('the_right_from_table','dt=20200101')
 
 table_left.select(['name1','a','namea','nameb'])\
 .where('a>0')\
@@ -148,4 +170,4 @@ table_left.select(['name1','a','namea','nameb'])\
 .rename_multiple([['namea','namec'],['nameb','named']])\
 .UDF('UDFc',['a','b'],'c')\
 .UDF_multiple([['UDF1',['a','b'],'c'],['UDF2',['p','q'],'r']])\
-.get_sql(drop=True)
+.create_table('table_final',drop=True)
